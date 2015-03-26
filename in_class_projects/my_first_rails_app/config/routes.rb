8ffd2f5188bb2a84c4de638df1779251464e613b
Grace Yu <grace.yu4444@gmail.com>
2015-03-25 19:54:11 -0600

    update

--- in_class_projects/my_first_rails_app/config/routes.rb
+++ in_class_projects/my_first_rails_app/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:name' => 'welcome#hello'
+
+  get 'time/now' => 'time#now'
+
+  get 'introduce/:name1/and/:name2' => 'introduce#names'
 end
